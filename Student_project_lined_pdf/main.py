b6e44aacf13c056c2e451079f2f0f3c59cbbcf38
Cornel Earle <csaearle38@gmail.com>
2023-09-22 08:04:48 +0100

    #  add footer + draw multiple line occupying entire pages

--- Student_project_lined_pdf/main.py
+++ Student_project_lined_pdf/main.py
@@ -22,6 +22,18 @@ for index, row in df.iterrows():
     # Draw a line under the header
     pdf.line(lines_x1, line_y1, lines_x2, line_y2)
 
+    #  Draw multiple lines under the header of first page
+
+    for lines_vertical_p1 in range(line_y1, 270, 10):
+        print(lines_vertical_p1)
+        pdf.line(lines_x1, lines_vertical_p1, lines_x2, lines_vertical_p1)
+
+    # Write in the footer on first page of each Topic
+    pdf.ln(241)
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.set_text_color(50, 100, 255)
+    pdf.cell(txt=row["Topic"], align='R', w=0, h=10, ln=1)
+
 
 
 
@@ -34,10 +46,18 @@ for index, row in df.iterrows():
         # Draw a line under the header of subsequent pages
         pdf.line(lines_x1, line_y1, lines_x2, line_y2)
 
-        # Draw the lines of 10mm apart from first line down to footer
-  #      pdf.line(lines_x1, line_y1 + 10 line_y2 + 10 )
+        # Draw the lines of 10mm apart from first line down to footer vertically
+        for lines_vertical in range(line_y1, 270, 10):
+            print(lines_vertical)
+            pdf.line(lines_x1, lines_vertical, lines_x2, lines_vertical)
 
-        print(pages)
+        # Write the footer under the multiple lines
+        pdf.ln(241)
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.set_text_color(50, 100, 255)
+        pdf.cell(txt=row["Topic"], align='R', w=0, h=10, ln=1)
+
+    # print(pages)
 
 # print out the pdf file, as formatted
 pdf.output("student_output.pdf")
